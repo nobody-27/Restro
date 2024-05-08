4e87c1d9ed74f252fb57720c457a0babafc7f9bf
nobody-27 <nobodypal27@gmail.com>
2024-05-09 00:03:20 +0530

    fixed templates issues

--- authentication/views.py
+++ authentication/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request,'email/test.html')
+# def home(request):
+#     return render(request,'email/test.html')
--- restro/urls.py
+++ restro/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from authentication.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/authentication/',include('authentication.api.urls')),
-    path('',home,name="home")
     
 ]
